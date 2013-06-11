03e2dbfecfdb6b223cbec28afa1186cd65d5839b
qingliu <qingliu@gmail.com>
2013-06-11 22:52:49 +0800

    Exercise generate api documentation

---
+++ JavadocTest.java
@@ -0,0 +1,15 @@
+package com.shuiqingliu;
+public class JavadocTest
+{
+	/**
+	*简单测试Field
+	*/
+	protected String name;
+	/**
+	*主方法,程序的入口
+	*/
+	public static void main (String args[])
+	{
+		System.out.println("Hello qingliu!");
+	}
+}
---
+++ Test.java
@@ -0,0 +1,24 @@
+package com.qingliu;
+	/**
+	*Description:
+	*<br/> 网站:<a href="http://www.shuiqingliu.com">qingliu blog </a>
+	*<br/> Copyright (c), 2012-2013 , qingliu
+	*<br/> This program is protected by Copyright laws.
+	*<br/> Program Name:
+	*<br/>Date:
+	*@auther qingliu  shuiqingliu14@gmail.com
+	*@version 1.0
+	 */
+public class Test
+{
+	/**
+	*简单测试Field
+	*/
+	public int age;
+	/**
+	*Test类 的测试构造器
+	*/
+	public Test()
+	{
+	}
+}
