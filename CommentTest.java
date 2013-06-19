33a2be902b33c2787a869490e19e2a8736f928c3
qingliu <shuiqingliu14@gmail.com>
2013-06-20 01:09:18 +0800

    Learning java annotations
    a simple program

---
+++ CommentTest.java
@@ -0,0 +1,12 @@
+public class CommentTest
+{
+	/*
+	这里面的内容都是多行注释
+	java语言真的很有趣
+	*/
+	public static void main (String[] args)
+	{
+		System.out.println("Hello qingliu !");
+		//System.out.println("这行代码被注释掉了，将不会被编译执行。")
+	}
+}
