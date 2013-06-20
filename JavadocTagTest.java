6555791fd376dd034ab5740c48d74e94b3b1a8ac
qingliu <shuiqingliu14@gmail.com>
2013-06-20 23:39:17 +0800

    学习使用java的api文档
    运用java的标记示范 eg @author @return @param name

---
+++ JavadocTagTest.java
@@ -0,0 +1,23 @@
+package qingliu;
+/**
+*Description:
+* <br/>网站:<a href="http://www.shuiqingliu.com">qingliu's blog</a>
+* <br/> Copyright (C) ,2012-2013,Shui qingliu
+* <br/>This program is protected by copyright laws.
+* <br/>Programe Name:
+*<br/>Date:
+* @author Shuiqingliu shuiqingliu14@gmail.com
+* @version 1.0
+*/
+public class JavadocTagTest
+{
+	/**
+	*一个得到打招呼字符串的方法
+	* @param name 该参数指定向谁打招呼
+	* @return 返回打招呼的字符串
+	*/
+	public String hello(String name)
+	{
+		return name + ",你好！" ;
+	}
+}
