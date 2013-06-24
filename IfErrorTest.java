9040c144b6d66c2b10df98e488a1dde85cf8454e
qingliu <shuiqingliu14@gmail.com>
2013-06-24 22:15:09 +0800

    逻辑错误

---
+++ IfErrorTest.java
@@ -0,0 +1,20 @@
+public class IfErrorTest
+{
+	public static void main(String[] args)
+	{
+		int  age  = 45;
+		if (age > 20)
+		{
+			System.out.println("青年人");
+		}
+		else if (age >  40)
+		{
+			System.out.println("中年人");
+
+		}
+		else if (age > 60)
+		{
+ 				System.out.println("老年人");
+ 			}
+	}
+}
