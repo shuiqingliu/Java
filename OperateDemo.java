540429d24075e507161ce1cd8bccd34908f36d01
qingliu <shuiqingliu14@gmail.com>
2013-07-01 20:58:29 +0800

    Operate

---
+++ OperateDemo.java
@@ -0,0 +1,11 @@
+public class OperateDemo
+{
+     public static void main(String[] args)
+	{
+		int num = 22;
+		System.out.println("第一次输出： num = " + num);
+		num = num - 3; //改变变量的内容
+		System.out.println("第二次输出： num = " + num);
+		
+	}
+}
---
+++ OperateDemo2.java
@@ -0,0 +1,13 @@
+﻿public class OperateDemo2
+{
+	public static void main(String[] args)
+	{
+		boolean b = false;
+		int x = 10;
+		int y = -30;
+		System.out.println("b = " + b + ", !b = " + !b);
+		System.out.println("x = " + x + ", -x = " + -x );
+		System.out.println("y = " + y + ", +y = " + +y);
+	}
+	
+}
