f9fd22278cd7cd3a8de34da22addce23d80233ed
qingliu <shuiqingliu14@gmail.com>
2013-08-30 17:15:40 +0800

    Objects, Classes

---
+++ Song.java
@@ -0,0 +1,23 @@
+public class Song{
+	String title;
+	String artist;
+
+	public void setTitle(String title){
+		this.title = title;	
+	}
+
+	public void setArtist(String artist){
+		this.artist = artist;
+	}
+
+	public void play(){
+		System.out.println(artist + " is play " + title);
+	}
+
+	public static void main(String[] args){
+		Song mysong = new Song();
+		mysong.setTitle("yesterday once more");
+		mysong.setArtist("qingliu");
+		mysong.play();
+	}
+}
