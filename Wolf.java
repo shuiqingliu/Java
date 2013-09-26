db7b8b5c41310de0d08bb62435ecf398604b772f
qingliu <shuiqingliu14@gmail.com>
2013-09-26 22:52:41 +0800

    extends

--- Wolf.java
+++ Wolf.java
@@ -1,48 +1,37 @@
 class Animal {
-    String name; 
-    String food; 
+    String name;
+    String food;
     int hunger;
 
     void makeNoise() { }
-    void eat() { }  
-    void sleep() { } 
-    void roam() { }  
+    void eat() { }
+    void sleep() { }
+    void roam() { }
 }
 
-class Feline extends Animal {
+class Canine extends Animal {
     void roam() {
-        System.out.println("猫科动物的活动方式");
+        System.out.println("犬科动物的活动方式");
     }
 }
 
-class Lion extends Feline {
+public class Wolf extends Canine {
     void makeNoise() {
-        System.out.println("狮子吼");
+        System.out.println("呜呜呜...");
     }
 
     void eat() {
-        System.out.println("狮子吃肉咔哧咔哧");
+        System.out.println("狼吃肉，好恐怖！");
     }
-}
-//在这里设计犬科动物和狼
-class Canine extends Animal{
-    void roam(){
-        System.out.println("犬科动物活动方式");
-    }
-}
 
-public class Wolf extends Canine{
-    void makeNoise(){
-        System.out.println("呜呜呜");   
-    }
-    
-    void eat(){
-        System.out.println("狼吃肉");
-    }
-    public static void main(String[] args){
-    Wolf f = new Wolf();
-    f.makeNoise();
-    f.eat();
+    public static void main(String[] args) {
+        //在这里写代码
+        Wolf f = new Wolf();
+            f.makeNoise();
+            f.eat();
+            f.roam();
+            f.sleep();
+            
+        
     }
 }
-
