89ed1f352671e9683e44a7ff7b2d235b035a6e33
qingliu <shuiqingliu14@gmail.com>
2013-11-21 20:24:14 +0800

    powerful java 5.0

---
+++ MaoMao.java
@@ -0,0 +1,9 @@
+public class MaoMao {
+    public static void main(String[] args) {
+    //在这里写代码
+        String[] name = {"maomao1","maomao2","maomao3","maomao4"};
+        for(String n : name){
+            System.out.println(n);
+        }
+    }
+}
