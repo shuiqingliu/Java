a505b12d03459b33387df65fad774f36b3729706
qingliu <shuiqingliu14@gmail.com>
2014-01-29 04:27:36 +0800

    数组拷贝

---
+++ arraydemo/ArrayRefDemo05.java
@@ -0,0 +1,18 @@
+public class ArrayRefDemo05{
+	public static void main(String[] args){
+		int i1[] = {1,2,3,4,5,6,7,8,9};
+		int i2[] = {11,22,33,44,55,66,77,88,99};
+		copy(i1,3,i2,1,3);
+		print(i2);
+	}
+	public static void copy(int s[],int s1,int o[],int s2,int len){
+		for(int i = 0;i < len;i++){
+			o[s2 + i] = s[s1 + i];
+		}
+	}
+	public static void print(int temp[]){
+		for(int i = 0;i < temp.length;i++){
+			System.out.print(temp[i] + "\t");
+		}
+	}
+}
