620440acedad0efde57edc3dd0043f2f63e227c0
qingliu <shuiqingliu14@gmail.com>
2014-01-29 13:13:27 +0800

    截取字符串

---
+++ oo4/StringAPIDemo07.java
@@ -0,0 +1,7 @@
+public class StringAPIDemo07{
+	public static void main(String[] args){
+		String str1 = "hello world";
+		System.out.println(str1.substring(6));
+		System.out.println(str1.substring(0,5));
+	}
+}
