c977d7e0cdc9b6e12c99d8fb591fc9978b6442ad
qingliu <shuiqingliu14@gmail.com>
2014-01-29 13:04:23 +0800

    查找字符串中某个字符位置

---
+++ oo4/StringAPIDemo04.java
@@ -0,0 +1,7 @@
+public class StringAPIDemo04{
+	public static void main(String[] args){
+		String str1 = "hello qingliu";
+		System.out.println("\"" + str1 + "\"的长度为：" + str1.length());
+		System.out.println("\"");
+    }
+}
---
+++ oo4/StringAPIDemo05.java
@@ -0,0 +1,8 @@
+public class StringAPIDemo05{
+	public static void main(String[] args){
+		String str1 = "abcdefcgh";
+		System.out.println(str1.indexOf("c"));
+		System.out.println(str1.indexOf("c",3));
+		System.out.println(str1.indexOf("x"));
+	}
+}
