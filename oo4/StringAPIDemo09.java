3c5204550230253a312f7d3d151f3f8b49f5eec8
qingliu <shuiqingliu14@gmail.com>
2014-01-29 14:01:57 +0800

    字符串大小写转换toUpperCase(),toLowerCase()

---
+++ oo4/StringAPIDemo09.java
@@ -0,0 +1,6 @@
+public class StringAPIDemo09{
+	public static void main(String[] args){
+		System.out.println("将\"hello world\"转成大写:"  + "hello world".toUpperCase());
+		System.out.println("将\"hello world\"转成小写:"  + "HELLO WORLD".toLowerCase());	
+	}
+}
