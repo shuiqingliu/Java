593942aa047e1b93e29a91486536e15535fa3ffc
qingliu <shuiqingliu14@gmail.com>
2014-02-01 14:16:00 +0800

    static关键字(4)

---
+++ oo7/StaticDemo04.java
@@ -0,0 +1,35 @@
+class Person{
+	private String name;
+	private int age;
+	private static String country = "A城";
+	public static void setCountry(String c){
+		country = c;
+	}
+	public String getCountry(){
+		return country;
+	}
+	public Person(String name,int age){
+		this.name = name;
+		this.age = age;
+	}
+	public void info(){
+		System.out.println("姓名:" + this.name + ", 年龄:" + this.age + ", 城市:" + country);
+	}
+}
+public class StaticDemo04{
+	public static void main(String[] args){
+		Person per1 = new Person("张三",30);
+		Person per2 = new Person("李四",31);
+		Person per3 = new Person("王五",32);
+		System.out.println("--------------修改之前---------------");
+		per1.info();
+		per2.info();
+		per3.info();
+		//per1.country = "B城";
+		Person.setCountry("B城");
+		System.out.println("--------------修改之后---------------");
+		per1.info();
+		per2.info();
+		per3.info();
+	}
+}
