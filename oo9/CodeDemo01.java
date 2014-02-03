b48bdebbaa35b2d9a653dfb6b4a79a08ec131ecf
qingliu <shuiqingliu14@gmail.com>
2014-02-04 02:25:07 +0800

    普通代码块

---
+++ oo9/CodeDemo01.java
@@ -0,0 +1,11 @@
+public class CodeDemo01{
+	public static void main(String args[]){
+		{
+			int x = 3;
+			System.out.println("x的值是-->" + x);
+		}
+
+		int x = 100;
+		System.out.println("x的值是-->" +  x);
+	}
+}
