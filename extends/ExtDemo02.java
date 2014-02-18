88bd5c3e44125e6051458f1b09e0cdc2e89974af
qingliu <shuiqingliu14@gmail.com>
2014-02-18 22:41:28 +0800

    继承后扩展

---
+++ extends/ExtDemo02.java
@@ -0,0 +1,34 @@
+class Person{
+	private String name;
+	private int age;
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	public int getAge(){
+		return this.age;
+	}
+}
+class Student extends Person{
+	private String school;
+	public void setSchool(String school){
+		this.school = school;
+	}
+	public String getSchool(){
+		return this.school;
+	}
+}
+public class ExtDemo02{
+	public static void main(String[] args){
+		Student stu = new Student();
+		stu.setName("qingliu");
+		stu.setAge(22);
+		stu.setSchool("华科");
+		System.out.println("姓名:" + stu.getName() + ",年龄:" + stu.getAge() + ",学校:" + stu.getSchool());
+	}
+}
