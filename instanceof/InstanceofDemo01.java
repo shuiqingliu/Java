f3acfa3e5dbba23e0a17e7c04579e5d39e97b332
qingliu <shuiqingliu14@gmail.com>
2014-02-22 18:50:46 +0800

    instanceof关键字 判断对象是否为某个类的实例

---
+++ instanceof/InstanceofDemo01.java
@@ -0,0 +1,30 @@
+class A{
+	public void fun1(){
+		System.out.println("A -----> public void fun1(){}");
+	}
+	public void fun2(){
+		this.fun1();
+	}
+}
+class B extends A{
+	public void fun1(){
+		System.out.println("B ------> public void fun1(){}");
+	}
+	public void fun3(){
+		System.out.println("B ------> public void fun3(){}");
+	}
+}
+class C extends A{
+	public void fun1(){
+		System.out.println("C ------> public void fun1(){}");
+	}
+	public void fun5(){
+		System.out.println("C ------> public void fun5(){}");
+	}
+}
+public class InstanceofDemo01{
+	public static void main(String[] args) {
+		A a1 = new B();
+		System.out.println("A a1 = new B()" + (a1 instanceof A));
+	}
+}
