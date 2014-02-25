400f4c9d24489d544aca2f1736619326e947dc6d
qingliu <shuiqingliu14@gmail.com>
2014-02-25 18:38:18 +0800

    finally异常的统一出口

---
+++ ExceptionDemo/ExceptionDemo03.java
@@ -0,0 +1,22 @@
+public class ExceptionDemo03{
+	public static void main(String[] args){
+		System.out.println("***************** 计算开始 *****************");
+		int i = 0;
+		int j = 0;		
+		
+		try{
+			String str1 = args[0];
+			String str2 = args[1];
+			i = Integer.parseInt(str1);
+			j = Integer.parseInt(str2);
+			int temp = i /j;
+			System.out.println("计算结果是:" + temp);
+		}
+		catch(ArithmeticException e){
+			System.out.println("产生异常:" + e);
+		}finally{
+			System.out.println("不管是否出现异常都执行");
+		}
+		System.out.println("***************** 计算结束 *****************");
+	}
+}
