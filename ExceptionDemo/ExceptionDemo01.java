f2b6700fa71d83e401aef359cca3276b270de8c8
qingliu <shuiqingliu14@gmail.com>
2014-02-25 18:27:11 +0800

    异常---产生异常

---
+++ ExceptionDemo/ExceptionDemo01.java
@@ -0,0 +1,10 @@
+public class ExceptionDemo01{
+	public static void main(String[] args){
+		int i = 10;
+		int j = 0;
+		System.out.println("***************** 计算开始 *****************");
+		int temp = i / j;
+		System.out.println("计算结果是:" + temp);
+		System.out.println("***************** 计算结束 *****************");
+	}
+}
