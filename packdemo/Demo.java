5fada6ec06899d56c6ff7f83406ccee01992b015
qingliu <shuiqingliu14@gmail.com>
2014-02-26 19:37:55 +0800

    被外包访问的类需要定义成public

---
+++ packdemo/Demo.java
@@ -0,0 +1,6 @@
+package com.shuiqingliu.demo;
+public class Demo{
+	public String getInfo(){
+		return "hello world!!!";
+	}
+}
---
+++ packdemo/ImportDemo01.java
@@ -0,0 +1,7 @@
+package com.shuiqingliu.demo1;
+import com.shuiqingliu.demo.Demo;
+public class ImportDemo01{
+	public static void main(String[] args){
+		System.out.println(new Demo().getInfo());
+	}
+}
