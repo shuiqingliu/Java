028bec831ffbc7de6e28097671a0996aaa2e0cbb
qingliu <shuiqingliu14@gmail.com>
2014-02-26 19:45:48 +0800

    用"*"导入某包下的类

---
+++ packdemo/ImportDemo02.java
@@ -0,0 +1,7 @@
+package com.shuiqingliu.demo1;
+import com.shuiqingliu.demo.*;
+public class ImportDemo02{
+	public static void main(String[] args){
+		System.out.println(new Demo().getInfo());
+	}
+}
