e33e63bfada60d431b84fe07dc2151bd5ee3511b
qingliu <shuiqingliu14@gmail.com>
2014-02-26 20:13:01 +0800

    静态导入

---
+++ packdemo/Operate.java
@@ -0,0 +1,15 @@
+package com.shuiqingliu.e;
+public class Operate{
+	public static int add(int i,int j){
+		return i + j;	
+	}
+	public static int sub(int i,int j){	
+		return i - j;
+	}
+	public static int mul(int i,int j){	
+		return i * j;
+	}
+	public static int div(int i,int j){	
+		return i / j;
+	}
+}
---
+++ packdemo/StaticImportDemo.java
@@ -0,0 +1,10 @@
+package com.shuiqingliu.f;
+import static com.shuiqingliu.e.Operate.*;
+public class StaticImportDemo{
+	public static void main(String[] args){
+		System.out.println("3 + 3 =" + add(3,3));
+		System.out.println("3 - 3 =" + sub(3,3));
+		System.out.println("3 * 3 =" + mul(3,3));
+		System.out.println("3 - 3 =" + div(3,3));
+	}
+}
