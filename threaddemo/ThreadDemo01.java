dd139f87ac4f7eac3019b4bfc7f76df14087ef15
qingliu <shuiqingliu14@gmail.com>
2014-02-27 22:16:26 +0800

    多线程thread

---
+++ threaddemo/ThreadDemo01.java
@@ -0,0 +1,19 @@
+class MyThread extends Thread{
+	private String name;
+	public MyThread(String name){
+		this.name = name;
+	}
+	public void run(){
+		for(int i = 0;i<10;i++){
+			System.out.println(name + "运行, i =" + i);
+		}
+	}
+}
+public class ThreadDemo01{
+	public static void main(String[] args){
+		MyThread mt1 = new MyThread("线程A");
+		MyThread mt2 = new MyThread("线程B");
+		mt1.run();
+		mt2.run();
+	}
+}
