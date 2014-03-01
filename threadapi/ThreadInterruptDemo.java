6abd5c86e5e671c3bde6a0d4605b1cb954bbca30
qingliu <shuiqingliu14@gmail.com>
2014-03-02 00:45:23 +0800

    中断线程

---
+++ threadapi/ThreadInterruptDemo.java
@@ -0,0 +1,20 @@
+class MyThread implements Runnable{
+	public void run(){
+		System.out.println("1.进入run()方法");
+		try{
+			Thread.sleep(10000);
+			System.out.println("2.休眠完成");
+		}catch(InterruptedException e){
+			System.out.println("3.线程被终止");
+		}
+		System.out.println("4.run()方法执行完成");
+	}
+}
+public class ThreadInterruptDemo{
+	public static void main(String[] args){
+		MyThread mt = new MyThread();
+		Thread t = new Thread(mt,"线程");
+		t.start();
+		t.interrupt();
+	}
+}
