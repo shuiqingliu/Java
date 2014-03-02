6cb078d36fc9f3ae4e840941be1aa1e21e5b288c
qingliu <shuiqingliu14@gmail.com>
2014-03-02 22:00:59 +0800

    some changes of class defined

--- excedemo/ExecDemo02.java
+++ excedemo/ExecDemo02.java
@@ -1,7 +1,7 @@
-class MyThread implements Runnable{
+class TheThread implements Runnable{
 	private String name;
 	private int time;
-	public MyThread(String name,int time){
+	public TheThread(String name,int time){
 		this.name = name;
 		this.time = time;
 	}
@@ -17,9 +17,9 @@ class MyThread implements Runnable{
 }
 public class ExecDemo02{
 	public static void main(String[] args){
-		MyThread mt1 = new MyThread("线程A", 1000);
-		MyThread mt2 = new MyThread("线程B", 2000);
-		MyThread mt3 = new MyThread("线程C", 3000);
+		TheThread mt1 = new TheThread("线程A", 1000);
+		TheThread mt2 = new TheThread("线程B", 2000);
+		TheThread mt3 = new TheThread("线程C", 3000);
 		new Thread(mt1).start();
 		new Thread(mt2).start();
 		new Thread(mt3).start();
