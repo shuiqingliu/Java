c6f8ccd99fb847ea888b0a02b195a23675b8de0e
qingliu <shuiqingliu14@gmail.com>
2014-03-04 21:18:56 +0800

    线程不同步的时候残生的问题

--- syndemo/SynDemo02.java
+++ syndemo/SynDemo02.java
@@ -1,6 +1,12 @@
-class OneThread implements Runnable{
+class TwoThread implements Runnable{
 	private int ticket = 5;
 	public void run(){
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 		for (int i = 0; i < 1000; i++) {
 			if(ticket > 0){				
 				System.out.println("卖票：" + ticket--);
@@ -8,9 +14,9 @@ class OneThread implements Runnable{
 		}
 	}
 }
-public class SynDemo01{
+public class SynDemo02{
 	public static void main(String[] args) {
-		OneThread one = new OneThread();
+		TwoThread one = new TwoThread();
 		Thread t1 = new Thread(one);
 		Thread t2 = new Thread(one);
 		Thread t3 = new Thread(one);
