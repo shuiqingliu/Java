f69662203cc2d48cad43abee2b4d6ba117fbddaa
qingliu <shuiqingliu14@gmail.com>
2014-03-18 22:13:06 +0800

    datecomplete

--- api3/DateDemo07.java
+++ api3/DateDemo07.java
@@ -15,6 +15,17 @@ class DateTime{
 		buf.append(this.addZero(calendar.get(Calendar.MILLISECOND),3));
 		return buf.toString();
 	}
+	public String getDateComplete(){
+		StringBuffer buf = new StringBuffer();
+		buf.append(calendar.get(Calendar.YEAR)).append("年"); //增加年
+		buf.append(this.addZero(calendar.get(Calendar.MONTH),2)).append("月");
+		buf.append(this.addZero((calendar.get(Calendar.DAY_OF_MONTH)+1),2)).append("日 ");
+		buf.append(this.addZero(calendar.get(Calendar.HOUR_OF_DAY),2)).append("时");
+		buf.append(this.addZero(calendar.get(Calendar.MINUTE),2)).append("分");
+		buf.append(this.addZero(calendar.get(Calendar.SECOND),2)).append("秒");
+		buf.append(this.addZero(calendar.get(Calendar.MILLISECOND),3)).append("毫秒");
+		return buf.toString();
+	}
 	public String addZero(int num,int len){
 		StringBuffer s = new StringBuffer();
 		s.append(num);
@@ -25,9 +36,10 @@ class DateTime{
 		return s.toString();
 	}
 }
-public class DateDemo06{
+public class DateDemo07{
 	public static void main(String[] args){
 		DateTime dt = new DateTime();
-		System.out.println("系统日期" + dt .getDate());
+		System.out.println("系统日期 :" + dt .getDate());
+		System.out.println("系统日期 :" + dt .getDateComplete());
 	}
 }
