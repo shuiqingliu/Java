bb65f67a5b275cfd9d510b01f7613bbddb95d1a6
qingliu <shuiqingliu14@gmail.com>
2014-03-19 20:33:02 +0800

    math round(),pow(),

---
+++ math/MathDemo01.java
@@ -0,0 +1,9 @@
+public class MathDemo01{
+	public static void main(String[] args){
+		System.out.println("求平方根 :" + Math.sqrt(9.0));
+		System.out.println("求最小值 :" + Math.min(20,30));
+		System.out.println("求最大值 :" + Math.max(12.23,46));
+		System.out.println("2的3次方 :" + Math.pow(2,3));
+		System.out.println("四舍五入 :" + Math.round(4.23));
+	}
+}
