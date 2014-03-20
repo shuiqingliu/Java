1f02e93cdcccf5f46ce8dfcb3378306f49d4323f
qingliu <shuiqingliu14@gmail.com>
2014-03-20 21:24:57 +0800

    decimalformat   ###,###.###

--- Number/NumberDemo04.java
+++ Number/NumberDemo04.java
@@ -7,9 +7,15 @@ class FormatDemo{
 		System.out.println("使用" + pattern + "格式化" + value + "；" + str); 
 	}
 }
-public class NumberDemo03{
+public class NumberDemo04{
 	public static void main(String[] args){
 		FormatDemo fd = new FormatDemo();
 		fd.format1("###,###.###",111222.34565778);
+		fd.format1("000,000.000",11222.34565778);
+		fd.format1("000,000.000￥",11222.34565778);
+		fd.format1("000,000.000$",11222.34565778);
+		fd.format1("###.###%",0.34565778);
+		fd.format1("###.###\u2030",0.34565778);
+		fd.format1("00.###%",0.034565778);
 	}
 }
