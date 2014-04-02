f3db1e586da04b9b193bc18a56b96210abca74a6
qingliu <shuiqingliu14@gmail.com>
2014-04-02 20:55:06 +0800

    matcher replaceAll()

--- api7/RegexDemo05.java
+++ api7/RegexDemo05.java
@@ -1,14 +1,13 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-public class RegexDemo04{
+public class RegexDemo05{
 	public static void main(String[] args){
 		String str = "A002335B45000C34324D00123E210000F021H";       //此字符串由数字组成
 		String pat = "\\d+";
 		Pattern p = Pattern.compile(pat);
 		String s[] = p.split(str);
 		Matcher m = p.matcher(str);
-		for(int i=0;i<s.length;i++){
-			System.out.println(s[i] + "\t");
-		}
+		String newString = m.replaceAll("_");
+		System.out.println(newString);
 	}
 }
