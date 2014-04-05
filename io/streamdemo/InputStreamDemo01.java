029cbd25a1ef484d715836a23222a025bc1e77b7
qingliu <qingliu@live.cn>
2014-04-05 16:26:13 +0800

    InputStream

---
+++ io/streamdemo/InputStreamDemo01.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
+public class InputStreamDemo01{
+	public static void main(String[] args) throws Exception{
+		//第一步使用File类找到文件
+		File f = new File(File.separator + "home" + File.separator + 
+				"qingliu" + File.separator + "test.txt");
+		//第二步，通过子类实例化父类对象
+		InputStream input = null;             //准备好一个输出对象
+		input = new FileInputStream(f);       //通过对象多态性进行实例化
+		//第三步，进行读操作
+		byte b[] = new byte[1024]; 			 //所有内容读到此数组
+		input.read(b);	                     //读取内容
+		System.out.println("字符串为：" + new String(b));  //将字节转化为字符串
+		//第四步，关闭流
+		input.close();
+	}
+}
