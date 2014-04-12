352fb309438c50178db1aab7ad6541942c439dbc
qingliu <qingliu@live.cn>
2014-04-12 18:20:04 +0800

    set delimiter

--- io/scanner/ScannerDemo01.java
+++ io/scanner/ScannerDemo01.java
@@ -3,6 +3,7 @@ public class ScannerDemo01{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		System.out.print("请输入数据：");
+		scan.useDelimiter("\n");
 		String str = scan.next();
 		System.out.println("输入的数据为：" + str);
 	}
