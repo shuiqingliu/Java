d69fe3f001803611d0087967aa22db31f09bc154
qingliu <shuiqingliu14@gmail.com>
2014-04-15 20:26:39 +0800

    change to UTF-8

--- io/character encoding/CharSetDemo02.java
+++ io/character encoding/CharSetDemo02.java
@@ -6,7 +6,7 @@ public class CharSetDemo02{
 		/*实例化File类*/
 		File f = new File("d:" + File.separator + "test.txt");
 		OutputStream out = new FileOutputStream(f);			//实例化输出流
-		byte b[] = "你好,我叫xxx".getBytes("ISO8859-1");		//转码操作
+		byte b[] = "你好,我叫xxx".getBytes("UTF-8");		//转码操作
 		out.write(b);		//保存
 		out.close();		//关闭
 	}
