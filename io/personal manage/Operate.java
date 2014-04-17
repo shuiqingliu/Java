5fb2eea212b09f2b8c7efb22346e1dc70323d4f3
qingliu <shuiqingliu14@gmail.com>
2014-04-17 21:27:26 +0800

    write operate class

---
+++ io/personal manage/Operate.java
@@ -0,0 +1,14 @@
+public class Operate{
+	public static void add(){		//增加
+		System.out.println("增加操作");
+	}
+	public static void delete(){		//删除
+		System.out.println("删除操作");
+	}
+	public static void update(){		//更新
+		System.out.println("更新文件");
+	}
+	public static void find(){		//查看
+		System.out.println("查看信息");
+	}
+}
