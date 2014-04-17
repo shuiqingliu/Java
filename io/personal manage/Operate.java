c919343f5142cb4f291ae290a49b27d2e8e551da
qingliu <shuiqingliu14@gmail.com>
2014-04-17 21:57:39 +0800

    update Operate class

--- io/personal manage/Operate.java
+++ io/personal manage/Operate.java
@@ -1,6 +1,17 @@
 public class Operate{
 	public static void add(){		//增加
 		System.out.println("增加操作");
+		InputData input = new InputData();		//实例化输入数据对象
+		FileOperate fo = new FileOperate("d:\\test.txt");		//实例话FileOperate类
+		String name = input.getString("请输入姓名:");		//取得name;
+		int age = input.getInt("请输入年龄:","年龄必须为数字请重新输入");
+		Person per = new Person(name,age);		//实例化person对象
+		try{
+			fo.save(per);		//保存对象
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
 	}
 	public static void delete(){		//删除
 		System.out.println("删除操作");
@@ -10,5 +21,14 @@ public class Operate{
 	}
 	public static void find(){		//查看
 		System.out.println("查看信息");
+		FileOperate fo = new FileOperate("d:\\test.txt");		//实例话FileOperate类
+		Person per = null;
+		try{
+			per = fo.load();
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println(per);
 	}
 }
