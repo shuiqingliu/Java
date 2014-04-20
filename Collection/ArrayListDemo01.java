95b7fee30a02dc51c2c7654459c3adf75bff9cd8
qingliu <qingliu@live.cn>
2014-04-20 20:42:56 +0800

    Collection lList

---
+++ Collection/ArrayListDemo01.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayListDemo01{
+	public static void main(String[] args){
+		List<String> arrayList = null;
+		arrayList = new ArrayList<String>();		//指定操作的泛型为String
+		arrayList.add("Hello");		//add方法由collection接口而来
+		arrayList.add("World");		//默认情况下向最后加入
+		System.out.println(arrayList);
+	}
+}
